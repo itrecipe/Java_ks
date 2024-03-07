0db05bcb7133bb9a30474dbab19706c5f89203ad
ITRecipe <itrecipe95@gmail.com>
2024-03-07 19:04:58 +0900

    java_ks - exam
    test

---
+++ Test/src/com/exam/test/Test.java
@@ -0,0 +1,13 @@
+package com.exam.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("안녕?");
+		
+		for(int i=0; i < 5; i++) {
+			System.out.print("string");
+		}
+	}
+
+}
