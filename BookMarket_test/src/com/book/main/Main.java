0b21988933c641a0c16ab2b67c558b0b8a2f76bc
ITRecipe <itrecipe95@gmail.com>
2024-04-12 20:51:19 +0900

    java_exam
    24.04.12

---
+++ BookMarket_test/src/com/book/main/Main.java
@@ -0,0 +1,9 @@
+package com.book.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ BookMarket_test/src/com/book/object/Book.java
@@ -0,0 +1,7 @@
+package com.book.object;
+
+public class Book {
+	String id;
+	String name;
+	String price;
+}
